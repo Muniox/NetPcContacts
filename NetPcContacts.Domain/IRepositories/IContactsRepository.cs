2c8109a03a646fd338d09c16d6da685ee0af0be1
Paweł Bartoszewski <pawel.bartoszewski@lge.com>
2025-10-07 16:05:44 +0200

    Add repository pattern for managing Contact entities
    Introduced the `IContactsRepository` interface and its implementation `ContactsRepository` to manage `Contact` entities. Registered the repository in the dependency injection container.
    
    Updated `NetPcContacts.Domain.csproj` to include a new `Interfaces\` folder for organizing interface files. Added necessary `using` directives in `ServiceCollectionExtensions.cs` and registered the repository with `AddScoped`.
    
    Performed minor cleanup in `NetPcContactsDbContext.cs`. These changes improve modularity, testability, and maintainability of the codebase.

---
+++ NetPcContacts.Domain/IRepositories/IContactsRepository.cs
@@ -0,0 +1,18 @@
+﻿using NetPcContacts.Domain.Entities;
+
+namespace NetPcContacts.Domain.IRepositories
+
+{
+    public interface IContactsRepository
+    {
+        Task<int> Create(Contact contact);
+
+        Task Delete(Contact contact);
+
+        Task SaveChanges();
+
+        Task<IEnumerable<Contact>> GetAll();
+
+        Task<Contact?> GetById(int id);
+    }
+}
